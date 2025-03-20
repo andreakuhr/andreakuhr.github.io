c01e80af73453d00baa50a36fce3e2ef6e25ee6e
Andrea Uhr <andrea@getmatcha.com>
2025-03-20 10:05:35 -0400

    assets

--- blog/(subpages)/components/header.tsx
+++ blog/(subpages)/components/header.tsx
@@ -3,7 +3,11 @@ import Link from "next/link";
 export default function Header() {
   return (
     <header>
-      <Link href="/">this is a basic header</Link>
+      <div className="container mx-auto px-4">
+        <nav className="sticky top-0 flex">
+          <Link href="/">this is a basic header</Link>
+        </nav>
+      </div>
     </header>
   );
 }
